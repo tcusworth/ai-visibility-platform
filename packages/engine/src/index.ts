afedcd3b5ef5b42ed905d6781a0a8cc1017c4f7b
tcusworth <tcusworth@gmail.com>
2026-09-02 14:08:36 -0600

    Export scored execution service

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -2,3 +2,4 @@ export * from "./planning.js";
 export * from "./run-initializer.js";
 export * from "./execution-planner.js";
 export * from "./run-lifecycle.js";
+export * from "./scored-execution-service.js";
