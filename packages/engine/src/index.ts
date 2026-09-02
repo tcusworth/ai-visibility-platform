d59b76010dc03795c99a846267380a130efb9a5a
tcusworth <tcusworth@gmail.com>
2026-09-02 14:17:10 -0600

    Export provider-aware execution service

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -3,3 +3,4 @@ export * from "./run-initializer.js";
 export * from "./execution-planner.js";
 export * from "./run-lifecycle.js";
 export * from "./scored-execution-service.js";
+export * from "./benchmark-observation-execution-service.js";
